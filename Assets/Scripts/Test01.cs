a3a7b5286fcffbd5fd7474880eca7e67bb019613
kconcon3 <hkane3sub@excite.co.jp>
2018-11-06 19:01:48 +0900

    first commit

---
+++ Assets/Scripts/Test01.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Animatorに登録されているパラメータ(Parameters)を取得するサンプルコード
+/// </summary>
+public class Test01 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Animator anim = GetComponent<Animator>();
+
+#if true
+		// パラメータの総数を取ってインデックスでアクセス
+		for(int i = 0; i < anim.parameterCount; i++)
+		{
+			AnimatorControllerParameter param = anim.GetParameter(i);
+			Debug.Log(param.name);
+		}
+#else
+		// parametersの配列から取得
+		foreach (AnimatorControllerParameter param in anim.parameters)
+		{
+			Debug.Log(param.name);
+		}
+#endif
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # UnityAnimatorTest01
+
+## 概要  
+UnityのAnimatorに登録されているパラメータ(Parameters)を取得するテスト
+<br /> 
+## テストした時の環境  
+Windows7 Pro 64bit  
+Unity 2018.2.10f1  
+Visual Studio Community 2017  
+
